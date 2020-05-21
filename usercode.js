ce493d66ca0cec375d8d0b5bc0fe78f8ea54bff7
Alex.Grigurko <alex.grigurko@corezoid.com>
2020-05-21 11:56:45 +0300

    added

--- usercode.js
+++ usercode.js
@@ -1,27 +1,27 @@
 const https = require('https');
 
-const func = async (data) => {
-  return new Promise(resolve => {
+const func = (data) => {
+  return new Promise((resolve) => {
     https
-    .get('https://reqres.in/api/users?page=1', (resp) => {
-      let body = '';
-      resp.on('data', (chunk) => {
-        body += chunk;
+      .get('https://reqres.in/api/users?page=1', (resp) => {
+        let body = '';
+        resp.on('data', (chunk) => {
+          body += chunk;
+        });
+        resp.on('end', () => {
+          data.lang = 'java_script';
+          data.res = JSON.parse(body);
+        });
+      })
+      .on('error', (err) => {
+        data.err = err;
       });
-      resp.on('end', () => {
-        data.lang = 'java_script';
-        data.res = JSON.parse(body);
-      });
-    })
-    .on('error', (err) => {
-      data.err = err;
-    });
-    resolve(data)
-  })
+    resolve(data);
+  });
 };
 
 module.exports = (data) => {
-  func(data).then(data => {
-    return data
-  })
+  func(data).then((data) => {
+    return data;
+  });
 };
