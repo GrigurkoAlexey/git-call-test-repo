0bd9fb7699d80e3bf15f20f8c0715854837dafd9
Alex.Grigurko <alex.grigurko@corezoid.com>
2020-05-25 09:19:29 +0300

    adde

--- usercode.go
+++ usercode.go
@@ -17,11 +17,13 @@ func usercode(ctx context.Context, data map[string]interface{}) error {
 	}
 	defer res.Body.Close()
 
-	var response map[string]interface{} = json.NewDecoder(res.Body)
+	var response map[string]interface{}
+
+	json.NewDecoder(res.Body).Decode(response)
 
 	data["res"] = map[string]interface{}{
 		"code": res.StatusCode,
-		"body": Decode(response),
+		"body": response,
 	}
 	return nil
 }
